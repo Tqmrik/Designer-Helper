0da69ccffcfcfd471438fbc44be7897828f8b71a
Artem <steamxxlss@mail.ru>
2022-08-11 22:40:52 +0300

    Project tab layout

--- DevAddIns/IPropertiesForm/IPropertiesForm.Designer.cs
+++ DevAddIns/IPropertiesForm/IPropertiesForm.Designer.cs
@@ -31,11 +31,17 @@ namespace DevAddIns
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panelHolderForm = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.panelHolderForm);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -45,11 +51,38 @@ namespace DevAddIns
             // 
             // panelHolderForm
             // 
-            this.panelHolderForm.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelHolderForm.Location = new System.Drawing.Point(0, 0);
+            this.panelHolderForm.Location = new System.Drawing.Point(12, 12);
+            this.panelHolderForm.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
             this.panelHolderForm.Name = "panelHolderForm";
-            this.panelHolderForm.Size = new System.Drawing.Size(484, 611);
+            this.panelHolderForm.Size = new System.Drawing.Size(460, 560);
             this.panelHolderForm.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 580);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 581);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 580);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 21);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // IPropertiesForm
             // 
@@ -57,6 +90,7 @@ namespace DevAddIns
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 611);
             this.Controls.Add(this.panel1);
+            this.HelpButton = true;
             this.MaximumSize = new System.Drawing.Size(500, 650);
             this.MinimumSize = new System.Drawing.Size(500, 650);
             this.Name = "IPropertiesForm";
@@ -71,5 +105,8 @@ namespace DevAddIns
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panelHolderForm;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
